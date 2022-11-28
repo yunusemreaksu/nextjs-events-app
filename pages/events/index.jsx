0495c6d82d310140d4fa59d870bd14d6ad4c760d
Yunus Emre Aksu <emre_aksu@live.com>
2022-11-28 13:53:53 +0300

    Added filter form for events

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,11 +1,13 @@
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
   return (
-    <div>
+    <>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
