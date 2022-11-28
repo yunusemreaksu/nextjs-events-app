e1af5747c3bcdee70cecbe84db789fb693767241
Yunus Emre Aksu <emre_aksu@live.com>
2022-11-28 14:35:56 +0300

    Added functionality for navigating to the filtered events

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,20 @@
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 export default function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
